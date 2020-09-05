eeb662d41fc4566966c3f1259ad1674a3af02d57
AJBayliss81 <aj.bayliss@live.co.uk>
2020-09-05 16:59:07 +0000

    Added temporary css to '.temp-image' to keep image within '.image-box'

--- assets/js/animatch.js
+++ assets/js/animatch.js
@@ -1,7 +1,12 @@
-var timer = $("#start").click(function() {  
-    $(".image-box").html(`<img class="temp-image align-items-center justify-content-center" src='http://placegoat.com/300'/>`);
+var animalArray = ["assets/images/cat.jpg", "assets/images/dog.jpg", "assets/images/fox.jpg", "assets/images/goat.jpg"]
+
+$("#start").click(function() {  
+    $(".image-box").html(`<img class="temp-image align-items-center justify-content-center" src="assets/images/cat.jpg" />`);
+    $(".temp-image").css({"width": "350px", "height": "350px", "object-fit": "cover"});
+
     var randomImage = Math.floor(Math.random() * 4);
     console.log(randomImage);
+
     setTimeout(() => {
         $(".temp-image").remove();
     }, 3000);
