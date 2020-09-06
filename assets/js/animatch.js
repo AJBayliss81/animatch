ae8ca3820343f34ae65e61529321a4cf17e5ecbd
AJBayliss81 <aj.bayliss@live.co.uk>
2020-09-06 16:40:28 +0000

    Added random image generator function to 'animatach.js'

--- assets/js/animatch.js
+++ assets/js/animatch.js
@@ -1,14 +1,39 @@
-var animalArray = ["assets/images/cat.jpg", "assets/images/dog.jpg", "assets/images/fox.jpg", "assets/images/goat.jpg"]
+$("#start").click(function selectImage() {  
 
-$("#start").click(function() {  
-    $(".image-box").html(`<img class="temp-image align-items-center justify-content-center" src="assets/images/cat.jpg" />`);
+    var animalArray = ["assets/images/cat.jpg", "assets/images/dog.jpg", "assets/images/fox.jpg", "assets/images/goat.jpg"];
+    var randomNum = [Math.floor(Math.random() * animalArray.length)];
+    var randomPic = animalArray[randomNum];
+    var gameArray = [];
+
+    console.log(randomNum, randomPic);
+
+    $(".image-box").html(`<img class="temp-image align-items-center justify-content-center" src="${randomPic}"/>`);
     $(".temp-image").css({"width": "350px", "height": "350px", "object-fit": "cover"});
-
-    var randomImage = Math.floor(Math.random() * 4);
-    console.log(randomImage);
+    
 
     setTimeout(() => {
         $(".temp-image").remove();
     }, 3000);
 }); 
+
+$("#cat").click(function() {
+    
+    console.log(0);
+});
+
+$("#dog").click(function() {
+    
+    console.log(1);
+});
+
+$("#fox").click(function() {
+    
+    console.log(2);
+});
+
+$("#goat").click(function() {
+    
+    console.log(3);
+});
+
 
