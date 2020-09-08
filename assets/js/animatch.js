34a38bccff2ef932316ec1d7401055862b71e8d6
AJBayliss81 <aj.bayliss@live.co.uk>
2020-09-08 15:49:24 +0000

    Added class and onclick function to buttons in 'index.html' and added disabled button functionality and check array function to compare game and player arrays as well as beginning of progress to next level to 'animatch.js'.

--- assets/js/animatch.js
+++ assets/js/animatch.js
@@ -1,39 +1,70 @@
-$("#start").click(function selectImage() {  
+var gameArray = [];
+var playerArray = [];
+var animalArray = [
+    "assets/images/cat.jpg",
+    "assets/images/dog.jpg",
+    "assets/images/fox.jpg",
+    "assets/images/goat.jpg",
+  ]; // image array
 
-    var animalArray = ["assets/images/cat.jpg", "assets/images/dog.jpg", "assets/images/fox.jpg", "assets/images/goat.jpg"];
-    var randomNum = [Math.floor(Math.random() * animalArray.length)];
-    var randomPic = animalArray[randomNum];
-    var gameArray = [];
+function selectImage() {
 
-    console.log(randomNum, randomPic);
+  var randomNum = Math.floor(Math.random() * animalArray.length); // random no. generator
+  var randomPic = animalArray[randomNum]; // take pic from image array based on random no.
 
-    $(".image-box").html(`<img class="temp-image align-items-center justify-content-center" src="${randomPic}"/>`);
-    $(".temp-image").css({"width": "350px", "height": "350px", "object-fit": "cover"});
-    
+  gameArray.push(randomNum);
+  $(".button").attr("disabled", true).css("background-color", "red");
+  $(".image-box").html(
+    `<img class="temp-image align-items-center justify-content-center" src="${randomPic}"/>`
+  ); // generate new element for image
+  $(".temp-image").css({
+    width: "350px",
+    height: "350px",
+    "object-fit": "cover",
+  }); // change css to fit pic inside image box
 
-    setTimeout(() => {
-        $(".temp-image").remove();
-    }, 3000);
-}); 
+  // timer
+  setTimeout(() => {
+    $(".temp-image").remove(); // remove img element
+    $(".button").attr("disabled", false).css("background-color", "yellow");
+  }, 3000);
+  
 
-$("#cat").click(function() {
-    
-    console.log(0);
-});
+};
 
-$("#dog").click(function() {
-    
-    console.log(1);
-});
+// button click registers
+function cat() {
+  playerArray.push(0);
+}
 
-$("#fox").click(function() {
-    
-    console.log(2);
-});
+function dog() {
+  playerArray.push(1);
+}
 
-$("#goat").click(function() {
-    
-    console.log(3);
-});
+function fox() {
+  playerArray.push(2);
+}
 
+function goat() {
+  playerArray.push(3);
+}
+// check game array matches player array
+function checkArrays() {
+  if (gameArray.length !== playerArray.length) return false;
+  for (var i = 0; i < gameArray.length; ++i) {
+    if (gameArray[i] !== playerArray[i]) return false;
+  }
+
+  return true;
+}
+
+function nextLevel() {
+    if (checkArrays() !== true) {
+        gameArray = [];
+        alert ("Sorry, better luck next time!");
+    } else {
+        
+        alert ("Welcome to the next level!");
+    }
+};
 
