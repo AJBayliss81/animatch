7842275b886a77aac471e1b2d788714b29f97a75
AJBayliss81 <aj.bayliss@live.co.uk>
2020-09-13 21:43:37 +0000

    Added functionality to 'animatch.js' to present amount of images based on current level and fixed some bugs with matching player inputs to the images shown. Also added disabling/enabling of buttons based on the stage of the game to prevent clicking before the game is ready to proceed to both 'index.html' and 'animatch.js'.

--- assets/js/animatch.js
+++ assets/js/animatch.js
@@ -1,25 +1,22 @@
-var gameArray = [];
-var playerArray = [];
+var gameArray = []; // array to push randomly generated numbers to
+var playerArray = []; // array to store player inputs and to match with gameArray
 var animalArray = [
-    "assets/images/cat.jpg",
-    "assets/images/dog.jpg",
-    "assets/images/fox.jpg",
-    "assets/images/goat.jpg",
-  ]; // image array
+  "assets/images/cat.jpg",
+  "assets/images/dog.jpg",
+  "assets/images/fox.jpg",
+  "assets/images/goat.jpg",
+]; // image array
+
+var levelCounter = 0;
 
 function selectImage() {
-
   var randomNum = Math.floor(Math.random() * animalArray.length); // random no. generator
-  var randomPic = animalArray[randomNum]; // take pic from image array based on random no.
-
   gameArray.push(randomNum);
 
-  
-  if (gameArray.length == 1) {
+  var randomPic = animalArray[randomNum]; // take pic from image array based on random no.
 
-  $(".button").attr("disabled", true).css("background-color", "red");
   $(".image-box").html(
-    `<img class="temp-image align-items-center justify-content-center" src="${animalArray[randomNum]}"/>`
+    `<img class="temp-image align-items-center justify-content-center" src="${randomPic}"/>`
   ); // generate new element for image
   $(".temp-image").css({
     width: "350px",
@@ -28,17 +25,38 @@ function selectImage() {
   }); // change css to fit pic inside image box
 
   // timer
+
   setTimeout(() => {
     $(".temp-image").remove(); // remove img element
-    $(".button").attr("disabled", false).css("background-color", "yellow");
   }, 3000);
-  
-} else {
-    // for (var i = 0; i < gameArray.length; ++i)
-    return gameArray.forEach(selectImage());
-  }
+}
 
-};
+function imageReel() {
+  if (levelCounter == 0) {
+    selectImage();
+    levelCounter++;
+    $("#start").attr("disabled", true).css("background-color", "red");
+    setTimeout(() => {
+      $(".button").attr("disabled", false).css("background-color", "yellow");
+    }, 3000);
+  } else {
+    gameArray.splice(0);
+    levelCounter++;
+    function recall(func, num, delay) {
+      if (!num) return;
+      func();
+      setTimeout(function () {
+        recall(func, num - 1, delay);
+      }, delay);
+    }
+    recall(selectImage, levelCounter, 3500);
+    $("#start").attr("disabled", true).css("background-color", "red");
+    setTimeout(() => {
+      $(".button").attr("disabled", false).css("background-color", "yellow");
+    }, 3000 * levelCounter);
+  }
+  
+}
 
 // button click registers
 function cat() {
@@ -56,24 +74,27 @@ function fox() {
 function goat() {
   playerArray.push(3);
 }
+
 // check game array matches player array
 function checkArrays() {
   if (gameArray.length !== playerArray.length) return false;
   for (var i = 0; i < gameArray.length; ++i) {
     if (gameArray[i] !== playerArray[i]) return false;
   }
+  $("#start").attr("disabled", false).css("background-color", "yellow");
 
   return true;
 }
 
+// check the 2 arrays match to then progress to next level
 function nextLevel() {
-    if (checkArrays() !== true) {
-        gameArray = [];
-        alert ("Sorry, better luck next time!");
-    } else {
-        
-        alert ("Welcome to the next level!");
-    }
-};
-
-
+  if (checkArrays() !== true) {
+    playerArray.splice(0);
+    alert("Sorry, better luck next time!");
+    $(".button").attr("disabled", true).css("background-color", "red");
+  } else {
+    playerArray.splice(0);
+    alert("Welcome to the next level!");
+    $(".button").attr("disabled", true).css("background-color", "red");
+  }
+}
