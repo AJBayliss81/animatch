a415a4e2a20aecbc0ce03f238fa8f7f6382a25a2
AJBayliss81 <aj.bayliss@live.co.uk>
2020-09-14 13:30:54 +0000

    Added scoreboard table to 'index.html', fixed some bugs where buttons weren't enabling/disabling correctly, added points system and began to add the local storage for points to 'animatch.js. Also added some styling to 'styles.css' to create some space between buttons and lay the page out more.

--- assets/js/animatch.js
+++ assets/js/animatch.js
@@ -1,13 +1,28 @@
-var gameArray = []; // array to push randomly generated numbers to
-var playerArray = []; // array to store player inputs and to match with gameArray
-var animalArray = [
+/*----- Storage -----*/
+
+if (typeof (Storage) !== "undefined") {
+    $("#1st").text(localStorage.first);
+    $("#2nd").text(localStorage.second);
+    $("#3rd").text(localStorage.third);
+    $("#4th").text(localStorage.fourth);
+    $("#5th").text(localStorage.fifth);
+} else {
+    alert("<h5>Unable to save scores with current browser</h5>");
+}
+
+/*----- Global variables -----*/
+
+let gameArray = []; // array to push randomly generated numbers to
+let playerArray = []; // array to store player inputs and to match with gameArray
+let animalArray = [
   "assets/images/cat.jpg",
   "assets/images/dog.jpg",
   "assets/images/fox.jpg",
   "assets/images/goat.jpg",
 ]; // image array
 
-var levelCounter = 0;
+let levelCounter = 0;
+let points = 0;
 
 function selectImage() {
   var randomNum = Math.floor(Math.random() * animalArray.length); // random no. generator
@@ -19,8 +34,8 @@ function selectImage() {
     `<img class="temp-image align-items-center justify-content-center" src="${randomPic}"/>`
   ); // generate new element for image
   $(".temp-image").css({
-    width: "350px",
-    height: "350px",
+    width: "500px",
+    height: "500px",
     "object-fit": "cover",
   }); // change css to fit pic inside image box
 
@@ -92,9 +107,20 @@ function nextLevel() {
     playerArray.splice(0);
     alert("Sorry, better luck next time!");
     $(".button").attr("disabled", true).css("background-color", "red");
+    $("#start").attr("disabled", false).css("background-color", "yellow");
+    levelCounter = 0;
   } else {
     playerArray.splice(0);
     alert("Welcome to the next level!");
     $(".button").attr("disabled", true).css("background-color", "red");
+    points += (levelCounter * gameArray.length * 10);
+    console.log(points);
   }
 }
+
+// function updateScoreboard() {
+//     if(points > 0) {
+//         if (points > localStorage.first || localStorage.first == undefined)
+//         $("#1st").text("<td>${points}</td>");
+//     }
+// }
